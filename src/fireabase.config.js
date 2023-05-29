194d93dc08faf7ac9360c55ce170df1de0f58354
Hesham Gamal <hesham.gra@gmail.com>
2023-05-29 07:23:52 +0200

    last update

---
+++ src/fireabase.config.js
@@ -0,0 +1,37 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAMbiiquFsXyEc7h_gfNLB2Jo-p993kcvg",
+//   authDomain: "bazar-d3cb2.firebaseapp.com",
+//   projectId: "bazar-d3cb2",
+//   storageBucket: "bazar-d3cb2.appspot.com",
+//   messagingSenderId: "290833778260",
+//   appId: "1:290833778260:web:56aa636d4d6a3a38e48fc1",
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+//----------------------------------------------
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHUQ0tbJD2v9Rq2JrDgc2hp9ILmjpjd8k",
+  authDomain: "bazarapp-1163b.firebaseapp.com",
+  projectId: "bazarapp-1163b",
+  storageBucket: "bazarapp-1163b.appspot.com",
+  messagingSenderId: "21108805478",
+  appId: "1:21108805478:web:6f8072e8bcf5c21786c2a7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
